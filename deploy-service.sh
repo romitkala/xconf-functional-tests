55f9f3db677f885ed9429e27fdd308f2a4a3d330
Romit Kala <rkala@thoughtworks.com>
2014-08-30 10:40:23 +1000

    fixing deploy service

--- deploy-service.sh
+++ deploy-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 scp xconf-go-svc.rpm vagrant@$WEBSERVICEHOST:/tmp/
-ssh vagrant@$WEBSERVICEHOST 'sudo rpm -i xconf-go-svc.rpm'
+ssh vagrant@$WEBSERVICEHOST 'sudo rpm -i /tmp/xconf-go-svc.rpm'
 ssh vagrant@$WEBSERVICEHOST sudo /etc/init.d/xconf-go-svc start
