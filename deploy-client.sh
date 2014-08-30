339f10b5a5d3827a02faa29cbc785b4bb6be35bd
Romit Kala <rkala@thoughtworks.com>
2014-08-30 10:47:08 +1000

    take two client deploy

--- deploy-client.sh
+++ deploy-client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 scp xconf-svc-client.tgz vagrant@$WEBCLIENTHOST:/tmp/
-ssh vagrant@$WEBCLIENTHOST tar -xvf /tmp/xconf-svc-client.tgz -C /var/www/html/
+ssh vagrant@$WEBCLIENTHOST 'sudo tar -xvf /tmp/xconf-svc-client.tgz -C /var/www/html/'
